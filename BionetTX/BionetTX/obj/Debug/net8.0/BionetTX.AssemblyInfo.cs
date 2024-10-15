bf21a9087ba09f7ae95be64d7e2647ac3ef1c1b9
YujuKuo <yujuwk02@gmail.com>
2024-10-15 17:50:13 +0800

    index cn en 內容上新、排版完成

--- BionetTX/BionetTX/obj/Debug/net8.0/BionetTX.AssemblyInfo.cs
+++ BionetTX/BionetTX/obj/Debug/net8.0/BionetTX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f3d992c0efec5f3974bfc97de36b2073fddb89")]
 [assembly: System.Reflection.AssemblyProductAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
