651d21fc851f64ba4c25ab068c3fbf96e4786584
YujuKuo <yujuwk02@gmail.com>
2024-10-17 09:44:54 +0800

    中英文index內容放置完成，電腦、平版的nav第三層完成，手機版nav待修改

--- BionetTX/BionetTX/obj/Debug/net8.0/BionetTX.AssemblyInfo.cs
+++ BionetTX/BionetTX/obj/Debug/net8.0/BionetTX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f3d992c0efec5f3974bfc97de36b2073fddb89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf21a9087ba09f7ae95be64d7e2647ac3ef1c1b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
