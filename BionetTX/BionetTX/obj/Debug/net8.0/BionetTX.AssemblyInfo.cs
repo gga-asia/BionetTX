46906a282da141b3e32ffc525f590ee36e452ac4
YujuKuo <yujuwk02@gmail.com>
2025-01-16 09:36:09 +0800

    頁面樣式調整完成，GA4、GTM埋code位置測試

--- BionetTX/BionetTX/obj/Debug/net8.0/BionetTX.AssemblyInfo.cs
+++ BionetTX/BionetTX/obj/Debug/net8.0/BionetTX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46a1ef3b31ec6291535e6f95aba6ab45d484495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37906a89657048029cc17d1fe170cf1dd6509b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BionetTX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
